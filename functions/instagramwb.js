2cbde8476b48e294bed899bec9cae7738cb2c70c
Johan Huertas <89612924+joshuertasca@users.noreply.github.com>
2024-01-19 16:55:40 -0500

    Update instagramwb.js

--- functions/instagramwb.js
+++ functions/instagramwb.js
@@ -1,62 +1,46 @@
-/**
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
+const formattedReturn = require('./helpers/formattedReturn');
+const fetch = require('node-fetch');
+const { HttpStatusCode } = require('axios');
 
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
-var xhub = require('express-x-hub');
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
+exports.handler = async (event) => {
 
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
-app.use(bodyParser.json());
+    if (event.httpMethod === 'GET') {
 
-var token = process.env.TOKEN || 'token';
-var received_updates = [];
+       
+        return await formattedReturn(1158201444);
 
-app.get('/', function(req, res) {
-  console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
-});
+    }
+  
+    if (event.httpMethod === 'POST') {
 
-app.get(['/facebook', '/instagram'], function(req, res) {
-  if (
-    req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == token
-  ) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.sendStatus(400);
-  }
-});
+        const evento = JSON.parse(event.body);
 
-app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:', req.body);
 
-  if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.sendStatus(401);
-    return;
-  }
+        const bodyIngreso = {
+            "records": [{
+                "fields": {
+                    "Respuesta": JSON.stringify(evento),
+                }
+            }]
+        };
+        const EnviarAirtable = await fetch(process.env.REACT_APP_URL_AIRTABLE_EVENTOS_TELEGRAM, {
 
-  console.log('request header X-Hub-Signature validated');
-  // Process the Facebook updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
+            method: 'POST',
+            body: JSON.stringify(bodyIngreso),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
 
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
-  // Process the Instagram updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
 
-app.listen();
+        return await formattedReturn(200, { "status": "1", "reason": "Request Received" });
+
+
+
+
+
+    } else {
+        return formattedReturn(405, { "status": "-5", "reason": "Transacction Error" });
+    }
+};
